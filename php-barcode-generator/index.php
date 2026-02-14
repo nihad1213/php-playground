a4b26d9a58ab882af2c1e97c743ef46a45018294
nihad1213 <nihad.nemetli@gmail.com>
2026-02-14 15:41:57 +0400

    add: new functrions tested

--- php-barcode-generator/index.php
+++ php-barcode-generator/index.php
@@ -1,8 +1,15 @@
 <?php
 
 require './vendor/autoload.php';
+$colorRed = [255, 0, 0];
 
-$barcode = (new Picqer\Barcode\Types\TypeCode128())->getBarcode('08134234234');
+// $barcode = (new Picqer\Barcode\Types\TypeCode128())->getBarcode('08134234234');
 
-$renderer = new Picqer\Barcode\Renderers\HtmlRenderer();
-echo $renderer->render($barcode);
+// $renderer = new Picqer\Barcode\Renderers\HtmlRenderer();
+// echo $renderer->render($barcode);
+
+$barcode = (new Picqer\Barcode\Types\TypeCode128())->getBarcode('081231723897');
+$renderer = new Picqer\Barcode\Renderers\PngRenderer();
+$renderer->setForegroundColor($colorRed);
+
+file_put_contents('barcode.png', $renderer->render($barcode, $barcode->getWidth() * 3, 50));
